9d1e640640302159c9e7f697931a01d9ebc37c24
jasoomi <jasoomi045@gmail.com>
2025-08-15 18:57:06 +0500

    update

--- service-worker.js
+++ service-worker.js
@@ -52,3 +52,33 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+const CACHE_NAME = 'lifeskills-cache-v1';
+const urlsToCache = [
+  './lifeskills.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch cache first
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
